adfd92dd9d85e7e7be1f40b162d11c74cc0b43f5
DougLei <953034206@qq.com>
2020-04-15 17:17:28 +0800

    t

--- src/test/java/com/douglei/business/flow/ShoppingSettleAccounts.java
+++ src/test/java/com/douglei/business/flow/ShoppingSettleAccounts.java
@@ -14,7 +14,7 @@ public class ShoppingSettleAccounts {
 	@Test
 	public void test() {
 		BFFactory bf = new BFFactory(new JdbOrmDBSessionFactory(new XmlConfiguration().buildSessionFactory()));
-		BusinessFlow shoppingBF = bf.buildByResourceFile("ShoppingSettleAccounts.bf");
+		BusinessFlow shoppingBF = bf.buildByResourceFile("ShoppingSettleAccounts.bf.json"); // 后缀用.json, 是为了方便编辑配置文件
 		
 		Map<String, Object> inputValueMap = new HashMap<String, Object>();
 		inputValueMap.put("shoppingCarId", "1");
