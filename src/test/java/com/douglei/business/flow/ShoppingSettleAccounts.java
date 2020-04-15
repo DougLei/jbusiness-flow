12b02bf88082efebdc635d5758347bdca54d8b88
DougLei <953034206@qq.com>
2020-04-15 17:46:02 +0800

    t

--- src/test/java/com/douglei/business/flow/ShoppingSettleAccounts.java
+++ src/test/java/com/douglei/business/flow/ShoppingSettleAccounts.java
@@ -8,18 +8,18 @@ import org.junit.Test;
 import com.douglei.business.flow.db.def.impl.JdbOrmDBSession;
 import com.douglei.business.flow.executer.BusinessFlow;
 import com.douglei.orm.configuration.impl.xml.XmlConfiguration;
-import com.douglei.orm.sessionfactory.SessionFactory;
 
 public class ShoppingSettleAccounts {
 	
 	@Test
 	public void test() {
-		SessionFactory sf = new XmlConfiguration().buildSessionFactory();
+		JdbOrmDBSession session = new JdbOrmDBSession(new XmlConfiguration().buildSessionFactory());
+		
 		BFFactory bf = new BFFactory();
 		BusinessFlow shoppingBF = bf.buildByResourceFile("ShoppingSettleAccounts.bf.json");
 		Map<String, Object> inputValueMap = new HashMap<String, Object>();
 		inputValueMap.put("shoppingCarId", "1");
-		Map<String, Object> result = shoppingBF.execute(inputValueMap, new JdbOrmDBSession(sf));
+		Map<String, Object> result = shoppingBF.execute(inputValueMap, session);
 		System.out.println(result);
 	}
 }
