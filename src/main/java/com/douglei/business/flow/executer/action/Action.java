727b57521592b8137cb154d5d4b6debd9ecf58e5
DougLei <953034206@qq.com>
2020-04-26 16:30:17 +0800

    y

--- src/main/java/com/douglei/business/flow/executer/action/Action.java
+++ src/main/java/com/douglei/business/flow/executer/action/Action.java
@@ -1,6 +1,7 @@
 package com.douglei.business.flow.executer.action;
 
 import com.douglei.business.flow.db.DBSession;
+import com.douglei.business.flow.executer.DataType;
 import com.douglei.business.flow.executer.ParameterContext;
 import com.douglei.business.flow.executer.parameter.ResultParameter;
 
@@ -16,7 +17,17 @@ public abstract class Action {
 	 * @param value
 	 * @return 传入的value
 	 */
-	protected Object setResult(Object value) {
+//	protected Object setResult(Object value) {
+//		return setResult(value, null);
+//	}
+	
+	/**
+	 * 将结果set到参数map中
+	 * @param value
+	 * @param assignDataType 指定的数据类型, 如果传入null, 则使用result的类型
+	 * @return 传入的value
+	 */
+	protected Object setResult(Object value, DataType assignDataType) {
 		if(result != null)
 			ParameterContext.addParameter(result, value);
 		return value;
