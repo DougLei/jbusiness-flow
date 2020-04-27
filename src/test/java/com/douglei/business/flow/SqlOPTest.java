64c1158c5fd3b4dd5348ff498e114db6a762e1f8
DougLei <953034206@qq.com>
2020-04-27 10:12:43 +0800

    t

--- src/test/java/com/douglei/business/flow/SqlOPTest.java
+++ src/test/java/com/douglei/business/flow/SqlOPTest.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import com.alibaba.fastjson.JSONObject;
 import com.douglei.business.flow.db.def.impl.JdbOrmDBSession;
 import com.douglei.business.flow.executer.BusinessFlow;
 import com.douglei.orm.configuration.impl.xml.XmlConfiguration;
@@ -17,13 +18,16 @@ public class SqlOPTest {
 		BusinessFlow opBF = new BFFactory().buildByResourceFile("SqlOP.bf.json");
 		
 		Map<String, Object> inputValueMap = new HashMap<String, Object>();
-		inputValueMap.put("shoppingCarId", "testID");
+		inputValueMap.put("shoppingCarId", "1");
 		inputValueMap.put("productName", "测试");
 		inputValueMap.put("productType", 5);
 		inputValueMap.put("productPrice", 239.8);
 //		inputValueMap.put("productCount", 20);
 		
+		inputValueMap.put("pageNum", 2);
+		inputValueMap.put("pageSize", 1);
+		
 		Map<String, Object> result = opBF.execute(inputValueMap, session);
-		System.out.println(result);
+		System.out.println(JSONObject.toJSONString(result));
 	}
 }
